873deaad69b026647da50f4da578ba969bf73b7a
toshi <tab.hsts@gmail.com>
2021-08-13 16:11:57 +0900

    削除レイアウトへの遷移を実装

--- MemoApp/View/HomeView.swift
+++ MemoApp/View/HomeView.swift
@@ -11,6 +11,8 @@ import CoreData
 struct HomeView: View {
     // managedObjectContextをviewContextとして定義
     @Environment(\.managedObjectContext) private var viewContext
+    // メモ削除画面への切り替え
+    @State var isSelectingDeleteMemo = false
     // シート表示管理
     @State private var showSheet = false
     // プラスボタンのグラデーションの定義
@@ -30,10 +32,24 @@ struct HomeView: View {
     var body: some View {
         ZStack(alignment: .bottomTrailing){
             VStack(alignment: .leading) {
-                Text("メモの一覧")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.leading, 10)
+                HStack {
+                    Text("メモの一覧")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.leading, 10)
+                    // メモがあればメモ削除機能を使えるようにする
+                    if homeViewModel.memos.isEmpty == false {
+                        Spacer()
+                        Button(action: {
+                            isSelectingDeleteMemo.toggle()
+                        }) {
+                            Image(systemName: isSelectingDeleteMemo ? "clear" : "trash")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                                .padding(.trailing, 20)
+                        }
+                    }
+                }
                 //　メモがないときの画面表示
                 if homeViewModel.memos.isEmpty {
                     ZStack {
@@ -49,33 +65,58 @@ struct HomeView: View {
                 } else {
                     ScrollView(.vertical) {
                         LazyVStack(alignment: .leading) {
-                            ForEach(homeViewModel.memos) { memo in
-                                Text(memo.content!)
-                                    .font(.headline)
-                                    .padding(.horizontal, 5)
-                                Text(itemFormatter.string(from: memo.date!))
-                                    .font(.subheadline)
-                                    .padding(.horizontal, 5)
-                                // 区切り線
-                                Divider()
+                            ForEach(0 ..< homeViewModel.memos.count) { i in
+                                VStack(alignment: .leading){
+                                    Text(homeViewModel.memos[i].content!)
+                                        .font(.headline)
+                                        .padding(.horizontal, 5)
+                                    Text(itemFormatter.string(from: homeViewModel.memos[i].date!))
+                                        .font(.subheadline)
+                                        .padding(.horizontal, 5)
+                                    // 区切り線
+                                    Divider()
+                                }
                             }
                         }
                     }
                 }
-            }
-            // メモ登録シートを表示するボタン
-            Button(action: {
-                showSheet.toggle()
-            }) {
-                ZStack{
-                    Circle()
-                        .fill(plusButtonGradation)
-                        .frame(width: 70, height: 70, alignment: .center)
-                    Text("＋")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
+                // 削除メモ選択中であれば、削除ボタンを表示する
+                if isSelectingDeleteMemo {
+                    // 削除ボタン
+                    Button(action: {
+                        //　メモ削除
+                        homeViewModel.deleteMemo(viewContext: viewContext)
+                        
+                    }) {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.red)
+                                .frame(height: 50)
+                                .padding()
+                            Text("削除")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(.bottom, 30)
                 }
-                .offset(x: -30.0, y: -30.0)
+                // 削除メモ選択中でなければ、メモ登録シートを表示する
+                else {
+                    Button(action: {
+                        showSheet.toggle()
+                    }) {
+                        ZStack{
+                            Circle()
+                                .fill(plusButtonGradation)
+                                .frame(width: 70, height: 70, alignment: .center)
+                            Text("＋")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                        }
+                        .offset(x: -30.0, y: -30.0)
+                    }
+                }
             }
         }
         // シート表示
