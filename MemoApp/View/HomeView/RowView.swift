a1b27c928d4908986a807abf474005d5b91b6cc6
toshi <tab.hsts@gmail.com>
2021-08-29 02:09:05 +0900

    メモ削除時の暫定エラー対策

--- MemoApp/View/HomeView/RowView.swift
+++ MemoApp/View/HomeView/RowView.swift
@@ -23,30 +23,33 @@ struct RowView: View {
     }()
     
     var body: some View {
-        VStack(alignment: .leading){
-            HStack{
-                // 削除用のチェックボタンを表示する
-                if homeViewModel.isDeleteMode {
-                    Button(action: {
-                        homeViewModel.toggleMemoForDelete(index: index)
-                    }) {
-                        Image(systemName: homeViewModel.memos[index].isSelected ? "checkmark.square.fill" : "square")
-                            .padding(.leading, 10)
+        // メモ数未満のindexだけ表示する（削除時のエラー対策）
+        if index < homeViewModel.memos.count {
+            VStack(alignment: .leading){
+                HStack{
+                    // 削除用のチェックボタンを表示する
+                    if homeViewModel.isDeleteMode {
+                        Button(action: {
+                            homeViewModel.toggleMemoForDelete(index: index)
+                        }) {
+                            Image(systemName: homeViewModel.memos[index].isSelected ? "checkmark.square.fill" : "square")
+                                .padding(.leading, 10)
+                        }
+                    }
+                    // メモの内容と日付を表示
+                    VStack(alignment: .leading){
+                        Text(homeViewModel.memos[index].memo.content!)
+                            .lineLimit(2)
+                            .font(.headline)
+                            .padding(5)
+                        Text(itemFormatter.string(from: homeViewModel.memos[index].memo.date!))
+                            .font(.subheadline)
+                            .padding(.horizontal, 5)
                     }
                 }
-                // メモの内容と日付を表示
-                VStack(alignment: .leading){
-                    Text(homeViewModel.memos[index].memo.content!)
-                        .lineLimit(2)
-                        .font(.headline)
-                        .padding(5)
-                    Text(itemFormatter.string(from: homeViewModel.memos[index].memo.date!))
-                        .font(.subheadline)
-                        .padding(.horizontal, 5)
-                }
+                // 区切り線
+                Divider()
             }
-            // 区切り線
-            Divider()
         }
     }
 }
